e525ca28da1c37e6ec1c1edb939a822757c98b79
Arif Khan <ak5316221@gmail.com>
2025-07-31 12:03:14 +0100

    Frontend
    Added basic dashboard linked to a working backend system

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('Income', 'Income'),
+        ('Expense', 'Expense'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.type} - £{self.amount}"
--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
 
 # Create your views here.
+@login_required
+def dashboard(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+    income = sum(t.amount for t in transactions if t.type == 'Income')
+    expense = sum(t.amount for t in transactions if t.type == 'expense')
+    balance = income - expense
+
+    context = {
+        'transactions' : transactions,
+        'income' : income,
+        'expense' : expense,
+        'balance' : balance,
+    }
+
+    return render(request, 'finance/dashboard.html', context)
